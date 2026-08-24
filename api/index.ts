0b849af5d39772a42e439923d6b563100ed695fe
kunalsrivastava-dev <kunal.srivastava@manakinai.com>
2026-08-24 15:41:21 +0530

    Deployment fix

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { yoga } from "./document-vault/src/app.js";
+
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  return yoga(req, res);
+}
